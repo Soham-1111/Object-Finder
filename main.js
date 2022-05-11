71028b51b6cf7bfe40faafd6d20ea66517ab875b
Soham-1111 <73647733+Soham-1111@users.noreply.github.com>
2022-05-10 23:22:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
 function setup(){
-    canvas= createCanvas(400,300);
-    canvas.position(520,375); 
+    canvas= createCanvas(400,325);
+    canvas.position(520,360); 
+    video= createCapture(VIDEO); 
+    video.hide(); 
+}
+
+
+status=""; 
+ 
+
+
+
+function draw(){
+    image(video, 0,0,500,400 );
+ 
+}
+
+function start(){
+    objectDetector= ml5.objectDetector("cocossd", modelLoaded); 
+    document.getElementById("status").innerHTML= "Detecting Objects"; 
+
+}
+
+function modelLoaded(){
+    status="true"; 
+    console.log("Model Loaded!"); 
+    video.loop(); 
+    video.speed(1); 
+    video.volume(0); 
+}
+
+
+function pause(){
+    video.pause();
+}
+
+function stop(){
+    video.stop(); 
 }
